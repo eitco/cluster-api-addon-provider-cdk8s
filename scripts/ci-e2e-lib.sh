1deb1b8386fd537c2b070657ea0c8648fbbb99bc
PatrickLaabs <patrick.laabs@me.com>
2025-07-17 12:18:05 +0200

    last migration steps, prep for new release

--- scripts/ci-e2e-lib.sh
+++ scripts/ci-e2e-lib.sh
@@ -22,7 +22,7 @@ capi:buildDockerImages () {
   # please ensure the generated image name matches image names used in the E2E_CONF_FILE;
   # also the same settings must be set in Makefile, docker-build-e2e target.
   ARCH="$(go env GOARCH)"
-  export REGISTRY=ghcr.io/patricklaabs/cluster-api-addon-provider-cdk8s
+  export REGISTRY=ghcr.io/eitco/cluster-api-addon-provider-cdk8s
   export TAG=dev
   export ARCH
 
